d8add1574415d419f8c44830e59ba9910ff806a3
Kin Lane <kinlane@gmail.com>
2013-03-31 11:26:30 -0700

    try a new path

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -10,11 +10,6 @@ function listProviders()
     	
     	 $.each(data['tags'], function(key, val) {
 			
-			var template = $('#providerTagListingTemplate').html();
-			var title = Mustache.to_html(template, val);
-			
-			html += title;
-			
 			$tagArray[$tagCount] = val['tag'];	        	        		
 					
 			$tagCount++;
@@ -25,8 +20,44 @@ function listProviders()
         .done(function() {  
         
 			for (var i = 0; i < $tagArray.length; i++) {
-			    alert($tagArray[i]);
-			    //Do something
+				
+				$tag = $tagArray[i];				
+				
+			    $.getJSON('data/providers.json', function(providerdata) {
+			    	
+			    	$Any = 0;
+			    	$logos = '';
+			    	
+			    	 $.each(providerdata['serviceprovider'], function(key2, val2) {
+			    	 	
+			    	 	$tags = val2['tags'];
+			    	 	
+			    	 	$inside = $tags.indexOf($tag);
+			    	 	
+						//alert($category + ' in (' + $tags + ') ' + $inside);
+						
+						if($inside!=-1){
+							
+							var template2 = $('#providerListingTemplate').html();
+							
+							logo = Mustache.to_html(template2, val2);
+							
+							logos += logo;
+							 
+							}
+							
+						$Any = 1;	
+						 						
+				        });
+				        
+				       html +='<p><strong>{{tag}}</strong></p>' + logos; 
+				        
+			        }).done(function() { 				
+				
+				
+				alert(html);	
+				
+			   });
 			}            
 	
 		 });
