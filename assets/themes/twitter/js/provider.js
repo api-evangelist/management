6c1d30b0f44474df27cb5190a5656e5523ca8a0c
Kin Lane <kinlane@gmail.com>
2013-03-31 09:33:04 -0700

    update event JS

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -3,9 +3,27 @@ function listProviders()
     $.getJSON('data/tags.json', function(data) {
     	toggle = 0;
     	 $.each(data['tags'], function(key, val) {
+			
 			var template = $('#providerTagListingTemplate').html();
 			var html = Mustache.to_html(template, val);
-			$('#providerListing').append(html);    						
+			
+			$('#providerListing').append(html);   
+			
+			tag = val['tag'];
+			
+		    $.getJSON('data/providers.json', function(data2) {
+		    	toggle = 0;
+		    	 $.each(data['serviceprovider'], function(key2, val2) {
+		    	 	
+		    	 	tags = val['tag'];
+		    	 	
+					found = tags.indexOf(tag);
+					
+					alert('found:' + found);
+					 						
+			        });
+		        });			
+			 						
 	        });
         });
 
